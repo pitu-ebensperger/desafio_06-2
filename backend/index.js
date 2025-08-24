223f2652b319c31d69e4eae469e6f7e684e78927
pitu-ebensperger <pitu.ebensperger@gmail.com>
2025-08-23 22:10:22 -0400

    fixes

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,18 @@ const express = require('express') // importo express
 const app = express() // ejecuto express para ob
 const fs = require ('fs') // importo filesystem
 const cors = require('cors') // importo cors
-const path = require('path');
+const path = require('path'); // importo path
 
-
-app.listen(3000, console.log("¡Servidor encendido!")) // levanto servidor y identifico puerto
+const PORT = 3000 // defino puerto 
+app.listen(PORT, function(err){ // levanto servidor
+    if (err) { console.error("Error levantando el servidor"); return; }
+    console.log("Servidor encendido en el puerto", PORT)
+})
 
 app.use(cors()) 
 app.use(express.json()) // uso express.json para recibir datos en formato JSON
 
-function readCanciones(req, res) {
+function readCanciones() {
     try {
     return JSON.parse(fs.readFileSync('./repertorio.json', 'utf8'));
   } catch {
@@ -18,11 +21,6 @@ function readCanciones(req, res) {
   }
 }
 
-// GET/canciones
-app.get("/canciones", (req, res) => {
-const canciones = readCanciones();
-  res.json(canciones);
-});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
@@ -31,27 +29,38 @@ app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
+
+// GET/canciones
+app.get("/canciones", (req, res) => {
+const canciones = readCanciones();
+  res.json(canciones);
+});
+
+
 // POST/canciones 
 app.post("/canciones", (req, res) => {
 const cancion = req.body
 const canciones = readCanciones()
 canciones.push(cancion) //agrego cancion al array
 fs.writeFileSync('./repertorio.json', JSON.stringify(canciones)) //escribo el archivo
-res.send ('Canción agregada con éxito') //envío respuesta al cliente
+res.status(201).send ('Canción agregada con éxito') //envío respuesta al cliente
 });
+
+
+
 
 // PUT/canciones/:id
 app.put("/canciones/:id", (req, res) => {
 const {id} = req.params
-const cancion = req.body
+const cancion = req.body || {}; //si req.body undefined o null, asigno objeto vacío
 const canciones = readCanciones()
 const index = canciones.findIndex(p => p.id == id)
 if (index === -1) {
-    return res.status(404).send('Canción no encontrada')
+  return res.status(404).send('Canción no encontrada'); //si no existe, envío error 400
 }
-canciones[index] = cancion;
+canciones[index] = { ...canciones[index], ...cancion, id: canciones[index].id }; //uso operador spread juntando array y objeto nuevo, mantengo id original
 fs.writeFileSync('./repertorio.json', JSON.stringify(canciones)) //escribo el archivo
-res.send ('Canción actualizada con éxitnode index.jso') //envío respuesta al cliente
+res.status(200).send ('Canción actualizada con éxito') //envío respuesta al cliente
 });
 
 //DELETE/canciones/:id
@@ -60,7 +69,7 @@ const {id} = req.params
 const canciones = readCanciones()
 const index = canciones.findIndex(p => p.id == id)
 if (index === -1) {
-    return res.status(404).send('Canción no encontrada');
+    return res.status(404).send('Canción no encontrada'); //si no existe, envío error 400
   }
 canciones.splice(index, 1) 
 fs.writeFileSync('./repertorio.json', JSON.stringify(canciones)) //escribo el archivo
